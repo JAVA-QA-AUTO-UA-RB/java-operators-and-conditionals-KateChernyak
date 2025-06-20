0a5a7b9120c4dc0ad66c9c6e34d6f21e9c886fd3
qaCher87 <qachernyak@gmail.com>
2025-06-20 22:31:12 +0300

    My Version of Game

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,69 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        // НИЖЧЕ ПРИКЛАД КОДУ, ВИКОРИСТОВУЙТЕ ЙОГО ЯК ЗРАЗОК,
-        // АЛЕ САМ ПРИКЛАД МОЖНА ВИДАЛИТИ ПОТІМ
-        // Приклад сканнера для перехоплення вводу користувача з консолі
-        System.out.println("Привіт, відважний герою!");
-        System.out.println("Тебе чекає цікава подорож");
-        System.out.println("Ти йшов чарівною стежкою, і натрапив на роздоріжжя: 1 - піти наліво, 2 - піти направо");
+        System.out.println("Ласквао просимо до казкового лісу!");
+        System.out.println("Ваше завдання - пройти ліс, ухвалюючи правильні рішення");
+        System.out.println("На старті Здоров'я=100, Удача=50 ");
         Scanner sc = new Scanner(System.in);
-        // Таким чином можна отримати цифру яку ввів користувач
-        // і зберегти її в змінну типу int
+        Random random = new Random();
+        int health = 100;
+        int luck = 50;
+// Action 1
+        System.out.println("Ви йшли чарівною стежкою, і натрапили на роздоріжжя: 1 - піти старою стежкою, 2 - обрати скарб серед кущів ");
+
         int choice1 = sc.nextInt();
         if (choice1 == 1) {
-            System.out.println("Ти обрав піти наліво");
-            // відняти очки здоров*я
+            System.out.println("Ви обрали піти старою стежкою");
+            health -= 20;
+            System.out.println("Ви пішли старою стежкою і втратили 20 очок здоров’я.");
         } else if (choice1 == 2) {
-            System.out.println("Ти обрав піти направо");
-            // додати очки удачі
-        } else {
-            System.out.println("Ти заплутався");
-            // відняти очки здоров*я
+            System.out.println("Ви обрали скарб серед кущів та отримали 30 очок удачі");
+            luck += 30;
+            System.out.println("Ви отримали скарб та 30 очок удачі! ");
+        } else if (choice1 > 2) {
+            System.out.println("Ви заплуталися");
+            health -= 10;
+            System.out.println("Ви заплуталися і втратили 10 очок здоров’я.");
         }
-        // КІНЕЦЬ ПРИКЛАДУ, ВИДАЛІТЬ ПРИКЛАД, ЩОБ В КОДІ БУЛО ТІЛЬКИ ВАШЕ РІШЕННЯ
+// Action 2
+        System.out.println("Перед Вами постає загадковий колодязь");
+        System.out.println("Що Ви зробите? 1 - вип'єте магічної води, 2 - ігноруєте колодязь ");
 
+        int choice2 = sc.nextInt();
+        if (choice2 == 1) {
+            System.out.println("Ви обрали випити магічну воду");
+            health += 50;
+            System.out.println("Ви випили магічну воду з колодця та отримали 50 очок до здоров’я.");
+        } else if (choice2 == 2) {
+            System.out.println("Ви обрали ігнорувати колодязь");
+            luck -= 10;
+            System.out.println("Ви проігнорували магічну воду і втомились, тому втратили 10 очок удачі ");
+        } else if (choice2 > 2) {
+            System.out.println("Ви нічого не обрали");
+            System.out.println("Ваш стан здоров'я та удачі не змінився ");
 
-
-        // Нижче пишіть ваш код з реалізацією завдання
-        // Коли пишете логіку гри - не забувайте періодично запускати програму
-        // І переконуватись, що все працює як треба, заодно тестуйте її перебираючи інпути
-
+        }
+// Action 3
+        System.out.println(" Ви зустріли на шляху чарівну істоту, яка перевірить Вашу талан");
+        System.out.println("Чарівна істота пильно вдивляється у Ваші очі та обирає дію");
+        int choiceOfCreature = random.nextInt(3) + 1;
+        if (choiceOfCreature == 1) {
+            System.out.println("Істота дружня і ділиться з Вами їжею");
+            health += 20;
+        } else if (choiceOfCreature == 2) {
+            System.out.println("Істота ворожа та атакує, при цьому забирає 30 очок здоров’я.");
+            health -=30;
+        } else if (choice2 == 3) {
+            System.out.println("Істота байдужа до Вас і нічого не змінює");}
+// Player Status Chek
+        System.out.println("Поточний стан Вашого здоров'я: " + health);
+        System.out.println("Поточний стан Вашої Удачі: " + luck);
+        if (health <= 0 || luck <= 0) {
+            System.out.println("Ви не змогли пройти ліс... Спробуйте ще раз!");
+        } else {
+            System.out.println("Вітаємо! Ви змогли пройти через чарівний ліс!");
+        }
     }
-
 }
